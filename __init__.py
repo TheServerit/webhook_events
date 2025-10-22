c9d68deb70205986a8e8fc1366f442c82dbcd4fc
theserverit <henkinitzan@gmail.com>
2025-10-22 22:35:04 +0300

    Reformatted docstring of __init__

--- __init__.py
+++ __init__.py
@@ -1,8 +1,14 @@
 """
-### Made by [TheServerit](https://github.com/TheServerit)
+### A Python wrapper for Discord's [Webhook Events](https://discord.com/developers/docs/events/webhook-events#webhook-events).
 
-FULL WEBHOOK EVENTS DOCUMENTATION: https://discord.com/developers/docs/events/webhook-events
+Copyright: 2025-present [TheServerit](https://github.com/TheServerit)
+Licensed under MIT, see LICENSE for more details.
 """
+
+__title__ = "webhook_events"
+__author__ = "TheServerit"
+__license__ = 'MIT'
+__copyright__ = "Copyright 2025-present TheServerit"
 
 from ._internal.handler import Application, start_listening
 from . import events
