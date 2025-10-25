4c791a436ac4dd62b3cc1c5974520f588ed799b4
theserverit <henkinitzan@gmail.com>
2025-10-25 23:49:36 +0300

    Removed unecessary __attrs__ of package info from __init__.py

--- __init__.py
+++ __init__.py
@@ -5,11 +5,6 @@ Copyright: 2025-present [TheServerit](https://github.com/TheServerit)
 Licensed under MIT, see LICENSE for more details.
 """
 
-__title__ = "webhook_events"
-__author__ = "TheServerit"
-__license__ = 'MIT'
-__copyright__ = "Copyright 2025-present TheServerit"
-
 from ._internal.handler import Application, start_listening
 from . import events
 
