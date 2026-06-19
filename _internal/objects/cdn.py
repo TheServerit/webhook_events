230b3638a5cf6bef5c5dcef4dcf69043d0a5a1a4
theserverit <henkinitzan@gmail.com>
2026-06-19 19:07:43 +0300

    Reformat code of Asset

--- _internal/objects/cdn.py
+++ _internal/objects/cdn.py
@@ -22,11 +22,8 @@ class Asset:
     def __init__(self, asset_hash: str, obj_id: int | None, cdn_path: CDNPath | None) -> None:
         self.asset_hash = asset_hash
 
-        format = 'gif' if self.is_animated() else 'png'
-
-        id_part = None
-        if obj_id is not None:
-            id_part = f'{obj_id}/'
+        format = "gif" if self.is_animated() else "png"
+        id_part = '' if obj_id is None else f"{obj_id}/"
 
         if cdn_path is None:
             self.url = f"https://discord.com/assets/{self.asset_hash}.{format}"
@@ -34,7 +31,7 @@ class Asset:
             self.url = f"https://cdn.discordapp.com/{cdn_path}/{id_part}{self.asset_hash}.{format}"
 
     def is_animated(self) -> bool:
-        return self.asset_hash.startswith('a_')
+        return self.asset_hash.startswith("a_")
 
     def __repr__(self) -> str:
         return f"<Asset hash={self.asset_hash}>"
